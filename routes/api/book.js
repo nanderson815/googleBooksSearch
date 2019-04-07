6bb582b633e2563bac5648fbca3510e6afae84b7
Noah A <nanderson815@gmail.com>
2019-04-07 18:56:53 -0400

    routes done

--- routes/api/book.js
+++ routes/api/book.js
@@ -4,10 +4,18 @@ const Book = require("../../models/booksModel.js");
 // route is "/api/book"
 router.route("/").get((req, res) => {
     Book.find(req.query)
-    .then(dbModel => res.json(dbModel))
-}).post((req,res) => {
+        .then(dbModel => res.json(dbModel))
+}).post((req, res) => {
     Book.create(req.body)
-    .then(dbModel =>  res.json(dbModel))
+        .then(dbModel => res.json(dbModel))
 });
+
+router.route("/:id")
+    .delete((req, res) => {
+        Book.findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,13 +17,9 @@ if (process.env.NODE_ENV === "production") {
 // connect to the mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
+// Use all routes
 app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
